c8cfc2770de73b778e3d522be7c47a3f3cbdb9a4
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2017-10-31 20:20:34 +0300

    Initial Commit

---
+++ Computer Programming Mid Term Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Computer Programming Mid Term Project
+//
+//  Created by Muhammet Mehmet Emin Kartal on 10/31/17.
+//  Copyright © 2017 Muhammet Mehmet Emin Kartal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
